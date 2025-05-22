ebb84e8a4f31aa2591615777c8d151d8629b5d75
Baturhan <baturxan-06.72@mail.ru>
2025-05-22 21:56:20 +0600

    add useraccess methods

--- src/TaskManager/Interfaces/Services/IUserAccessService.cs
+++ src/TaskManager/Interfaces/Services/IUserAccessService.cs
@@ -5,6 +5,7 @@ namespace TaskManager.Interfaces.Services
     public interface IUserAccessService
     {
         IEnumerable<UserAccessDTO> GetUserAccesses(bool trackChanges);
+        IEnumerable<UserAccessDTO> GetUserAccessesByUserId(int userId, bool trackChanges);
         UserAccessDTO GetUserAccess(int userAccessId, bool trackChanges);
         void CreateUserAccess(UserAccessDTO userAccess);
         void UpdateUserAccess(int userAccessId, UserAccessDTO userAccess);
