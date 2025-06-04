cbe8ef0d2aac1593ae618f2626287aa0d2cf2d10
Baturhan <baturxan-06.72@mail.ru>
2025-06-04 22:48:18 +0600

    add create form

---
+++ src/TaskManager/Models/Shared/UserListDto.cs
@@ -0,0 +1,9 @@
+namespace TaskManager.Models.Shared
+{
+    public class UserListDto
+    {
+        public int Id { get; init; }
+        public string Username { get; init; }
+        public string Role { get; init; }
+    }
+}
